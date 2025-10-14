28ab737b27e3eb1ce1409f5237a64debac6d44bc
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-14 13:01:12 -0400

    Implement game logic to determine if the player or computer has won the round

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -5,7 +5,7 @@ let computerScore = 0;
 /**
  *  Returns a string representing one of three possible choices: rock, paper, or scissors.
  */
-function getComputerChoice(){
+function getComputerChoice() {
     const CHOICE = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * CHOICE.length);
 
@@ -13,11 +13,35 @@ function getComputerChoice(){
 }
 
 /**
- * 
  * Prompt the user for their choice and returns it.
  */
-function getHumanChoice(){
-    let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors')");
+function getHumanChoice() {
+    let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors')").toLowerCase();
 
     return humanChoice;
 }
+
+/**
+ * Takes the human and computer choices as inputs and returns the corresponding winner.
+ */
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Draw.")
+    }
+
+    else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore += 1;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+    }
+    else {
+        computerScore += 1;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
