483b959e2033551f804b455506b2bb6ad0e17037
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-14 12:00:30 -0400

    Create function to randomly generate the computer's choice

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+const CHOICE = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    var index = Math.floor(Math.random() * CHOICE.length);
+    return CHOICE[index]
+}
