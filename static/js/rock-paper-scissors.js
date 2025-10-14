4fe9a8292bed64697bd34dd7d8abd6cb31a51c8e
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-14 12:11:32 -0400

    Implement function to prompt user for their choice

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -1,6 +1,12 @@
 function getComputerChoice(){
     const CHOICE = ["rock", "paper", "scissors"];
-
     let index = Math.floor(Math.random() * CHOICE.length);
-    return CHOICE[index]
+
+    return CHOICE[index];
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors')");
+
+    return humanChoice;
 }
