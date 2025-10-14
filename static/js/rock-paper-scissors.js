097ecdd9c35fddae20f42bb988c60d70972824eb
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-14 12:20:29 -0400

    Add global variables to track player scores

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -1,3 +1,10 @@
+// global variables to track player scores
+let humanScore = 0;
+let computerScore = 0;
+
+/**
+ *  Returns a string representing one of three possible choices: rock, paper, or scissors.
+ */
 function getComputerChoice(){
     const CHOICE = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * CHOICE.length);
@@ -5,6 +12,10 @@ function getComputerChoice(){
     return CHOICE[index];
 }
 
+/**
+ * 
+ * Prompt the user for their choice and returns it.
+ */
 function getHumanChoice(){
     let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors')");
 
