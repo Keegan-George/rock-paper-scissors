62cc2490a4ada42a05bc3a5e8ad8270b45afb1cf
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-15 08:23:13 -0400

    Add function to control the game logic
    Create function to handle playing multiple rounds and update scores.
    Update docstring for each function.
    Reduce the playRound function to only handle determining a winner.

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -1,9 +1,5 @@
-// global variables to track player scores
-let humanScore = 0;
-let computerScore = 0;
-
 /**
- *  Returns a string representing one of three possible choices: rock, paper, or scissors.
+ *  Returns a random string representing one of three possible computer choices: rock, paper, or scissors.
  */
 function getComputerChoice() {
     const CHOICE = ["rock", "paper", "scissors"];
@@ -13,35 +9,69 @@ function getComputerChoice() {
 }
 
 /**
- * Prompt the user for their choice and returns it.
+ * Prompt the player for their choice and return it.
  */
 function getHumanChoice() {
-    let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors')").toLowerCase();
+    let humanChoice = prompt("Make your choice (Type 'rock', 'paper', or 'scissors'.)").toLowerCase();
 
     return humanChoice;
 }
 
 /**
- * Takes the human and computer choices as inputs and returns the corresponding winner.
+ * Handles the win logic by comparing the choices of the computer and the player. 
+ * Returns an integer (-1, 0, or 1) if the player has drawn, lost, or won accordingly against the computer.
  */
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("Draw.")
+        return -1;
     }
 
     else if ((humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")) {
-        humanScore += 1;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        return 1;
     }
     else {
-        computerScore += 1;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        return 0;
     }
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+/**
+ * Runs the game for the specified number of rounds, tracking scores and displaying results to the screen. 
+ */
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+    const ROUNDS = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < ROUNDS; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        result = playRound(humanSelection, computerSelection);
+
+        switch (result) {
+            case -1:
+                console.log("Draw.")
+                break;
+
+            case 1:
+                console.log(`You win! ${humanSelection} beats ${computerSelection}.`)
+                humanScore++;
+                break;
+
+            case 0:
+                console.log(`You lose! ${computerSelection} beats ${humanSelection}.`)
+                computerScore++;
+                break;
+        }
+
+        console.log(`Current score | player: ${humanScore}    computer: ${computerScore}`)
+    }
+
+}
+
+
+playGame();
+
