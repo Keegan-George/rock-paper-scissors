55aea8b7faf0e0b12bf04a7ba23649269350e116
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-21 19:36:53 -0400

    Add function to update the round winner and display it to the user

--- static/js/rock-paper-scissors.js
+++ static/js/rock-paper-scissors.js
@@ -22,22 +22,54 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper");
 
-    if (draw) return -1;
+    if (draw) return "Draw";
 
-    else if (win) return 1;
+    else if (win) return "You win!";
 
-    else return 0;
+    else return "You lose.";
 }
 
 /**
- * Launches the game, tracks scores, and displays results to the screen. 
+ * Displays the updated player choices to the screen. 
+ */
+function updateChoiceDisplay(playerChoice, computerChoice) {
+    const playerChoiceDisplay = document.querySelector(".player-choice");
+    const computerChoiceDisplay = document.querySelector(".computer-choice");
+
+    playerChoiceDisplay.textContent = playerChoice;
+    computerChoiceDisplay.textContent = computerChoice;
+}
+
+
+/**
+ * Displays the updated scores to the screen. 
+ */
+function updateScoreDisplay(playerScore, computerScore) {
+    const playerScoreDisplay = document.querySelector(".player-score");
+    const computerScoreDisplay = document.querySelector(".computer-score");
+
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+/**
+ * Displays the updated result of this round to the screen.
+ */
+function updateRoundResult(result) {
+    const resultDisplay = document.querySelector(".round-result");
+
+    resultDisplay.textContent = result;
+}
+
+/**
+ * Launches the game and tracks scores. 
  */
 
 function playGame() {
     // track scores
     let humanScore = 0;
     let computerScore = 0;
-    const TARGET_SCORE = 5;
+    const goal = 5;
 
     // button event listener
     const buttons = document.querySelectorAll("button");
@@ -46,61 +78,22 @@ function playGame() {
         button.addEventListener("click", () => {
             let humanChoice = button.textContent;
             let computerChoice = getComputerChoice();
-            
-            updateChoiceDisplay(humanChoice, computerChoice)
+
+            result = playRound(humanChoice, computerChoice);
+
+            if (result === 0) {
+                computerScore++;
+            }
+            else if (result === 1) {
+                humanScore++;
+            }
+
+            updateRoundResult(result);
+            updateChoiceDisplay(humanChoice, computerChoice);
             updateScoreDisplay(humanScore, computerScore);
         });
     });
 }
-
-function updateChoiceDisplay(playerChoice, computerChoice){
-    const playerChoiceDisplay = document.querySelector(".player-choice");
-    const computerChoiceDisplay = document.querySelector(".computer-choice");
-
-    playerChoiceDisplay.textContent = playerChoice;
-    computerChoiceDisplay.textContent = computerChoice;
-
-}
-
-function updateScoreDisplay(playerScore, computerScore){
-    const playerScoreDisplay = document.querySelector(".player-score");
-    const computerScoreDisplay = document.querySelector(".computer-score");
-
-    playerScoreDisplay.textContent = playerScore;
-    computerScoreDisplay.textContent = computerScore;
-}
-
-
-    // const ROUNDS = 5;
-
-    
-
-    // for (let i = 0; i < ROUNDS; i++) {
-    //     let computerSelection = getComputerChoice();
-
-    //     result = playRound(playerChoice, computerSelection);
-
-    //     switch (result) {
-    //         case -1:
-    //             console.log("Draw.")
-    //             break;
-
-    //         case 1:
-    //             console.log(`You win! ${playerChoice} beats ${computerSelection}.`)
-    //             humanScore++;
-    //             break;
-
-    //         case 0:
-    //             console.log(`You lose! ${computerSelection} beats ${playerChoice}.`)
-    //             computerScore++;
-    //             break;
-    //     }
-
-    //     console.log(`Current score | player: ${humanScore}    computer: ${computerScore}`)
-    // }
-
-
-
 
 playGame();
 
